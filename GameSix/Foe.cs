3a81c6070170632709d55967c33848b67a5e87d3
arbyun <someonealiveh@gmail.com>
2023-04-21 10:50:56 +0100

    feat: Implement GetName() and TakeDamage() methods

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -15,5 +15,27 @@
             health = 100;
             shield = 0;
         }
+        
+        public string GetName() => name;
+
+        /// <summary> Takes a float as an argument and subtracts it from the shield. If the shield is less than 0,
+        /// then it sets the health to 0.</summary>
+        /// <param name="dmg"> /// the amount of damage that the player takes. </param>
+        /// <returns> The shield and health.</returns>
+        public void TakeDamage(float dmg)
+        {
+            shield -= dmg;
+
+            if (shield < 0)
+            {
+                float dmgToTake = -shield;
+                shield = 0;
+                health -= dmgToTake;
+                if (health < 0)
+                {
+                    health = 0;
+                }
+            }
+        }
     }
 }
