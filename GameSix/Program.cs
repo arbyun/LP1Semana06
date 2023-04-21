8da9ed785551b4aed43cc4ab6e61ce41ab9f7b3a
arbyun <someonealiveh@gmail.com>
2023-04-21 11:22:05 +0100

    feat: Ask user number of foes to create

---
+++ GameSix/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many foes would you like to create?");
+            int n = Console.Read();
+            Foe[] allFoes = new Foe[n];
+        }
+    }
+}
