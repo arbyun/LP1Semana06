49101c7665c57c060e5581774592bb3b5eb605cd
arbyun <someonealiveh@gmail.com>
2023-04-21 11:47:14 +0100

    feat + fix: Fix error in for loop trying to append foe to allFoes and print each foe

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -7,20 +7,28 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many foes would you like to create?\n");
-            int n = Console.Read();
+            
+            Console.WriteLine("How many foes would you like to create?");
+            int n = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
             Foe[] allFoes = new Foe[n];
             
             Console.WriteLine("\nNow, I will walk you through the creation for each foe.\n\n");
 
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine($"Creating Foe {i} out of {n}...\n");
-                Console.WriteLine($"Please input a name for foe number {i}:\n");
+                Console.WriteLine($"Please input a name for foe number {0}:\n", i);
                 string foeName = Console.ReadLine();
 
-                Foe foe = new Foe("foeName");
-                allFoes.Append(foe);
+                Foe foe = new Foe(foeName);
+                allFoes[i - 1] = foe;
+            }
+
+            Console.WriteLine("\n\nFinal list of foes:\n");
+
+            foreach (Foe foe in allFoes)
+            {
+                Console.WriteLine($"Foe {foe.GetName()}, with {foe.GetHealth()} HP and {foe.GetShield()} shield.\n");
             }
         }
     }
