5c3d3112ec92e89c1d37e7d6807d951d8c5ffee8
arbyun <someonealiveh@gmail.com>
2023-04-21 10:39:04 +0100

    feat: Implement Foe constructor with default health and shield values

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,6 +2,18 @@
 {
     public class Foe
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        /// <summary> Constructor for the Foe class. Takes in a string name and sets the health to 100
+        /// and shield to 0.</summary>
+        /// <param name="name"> The name of the enemy</param>
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
