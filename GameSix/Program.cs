a11c06da24df345583433c90281ca5592d765670
arbyun <someonealiveh@gmail.com>
2023-04-21 11:31:48 +0100

    feat: Create for loop for creating and appending Foes to allFoes array

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace GameSix
 {
@@ -6,9 +7,21 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many foes would you like to create?");
+            Console.WriteLine("How many foes would you like to create?\n");
             int n = Console.Read();
             Foe[] allFoes = new Foe[n];
+            
+            Console.WriteLine("\nNow, I will walk you through the creation for each foe.\n\n");
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Creating Foe {i} out of {n}...\n");
+                Console.WriteLine($"Please input a name for foe number {i}:\n");
+                string foeName = Console.ReadLine();
+
+                Foe foe = new Foe("foeName");
+                allFoes.Append(foe);
+            }
         }
     }
 }
